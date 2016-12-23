1cd6ea0ae9eee36b9c16cb287134dd0f5d27f670
贺诗淳 <anhedoniahe@gmail.com>
2016-12-23 10:45:42 +0800

    更改了 readme 正式功能实现了

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 #ehentai_picker
+
+一个 ehentai 爬虫。纯属新人练手项目。
+
--- ehentai_2.py
+++ ehentai_2.py
@@ -15,9 +15,27 @@ def request(url):
     response = requests.get(url,headers = header)
     return response
 
+def mkdir(path):
+    isExists = os.path.exists(os.path.join("/Users/Anhedonia/Desktop/workspace/myproject/girlgirllove/", path))
+
+    if not isExists:
+        os.makedirs(os.path.join('/Users/Anhedonia/Desktop/workspace/myproject/girlgirllove/',path))
+        os.chdir('/Users/Anhedonia/Desktop/workspace/myproject/girlgirllove/'+path)
+    else:
+        os.chdir('/Users/Anhedonia/Desktop/workspace/myproject/girlgirllove/'+path)
+
 url = 'http://g.e-hentai.org/g/1008601/42b1fb2e82/'
 print('URL加载完成')
 html = request(url)
 print('requestget 完成')
-soup = BeautifulSoup(html.text , 'lxml').find('div' , class_ = 'gdtm')
-print(soup)
+img_soup_list = BeautifulSoup(html.text , 'lxml').find_all('div' , class_ = 'gdtm')
+for img_html in img_soup_list:
+    imgview_url = img_html.find('a')['href']
+    imgview = request(imgview_url)
+    img_url = BeautifulSoup(imgview.text , 'lxml').find('div',id='i3').find('img')['src']
+    img = request(img_url)
+    mkdir(url)
+    name = img_url[-8:-5]
+    f = open(name+'.jpg', 'ab')
+    f.write(img.content)
+    f.close()
